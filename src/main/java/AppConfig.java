042007269bb879b4e9e43f4773e4b9f60e758f56
KSK <Sp33d93@mail.ru>
2023-01-30 16:23:26 +0900

    Spring Bean Commit 1.0

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,14 +1,23 @@
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class AppConfig {
- 
+
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Scope("prototype")
+    @Bean(name="cat")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setMessage("Meow");
+        return cat;
     }
 }
